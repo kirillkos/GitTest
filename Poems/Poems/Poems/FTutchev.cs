09c1ab1f334a53024814215a892e2fc547541a41
Кирилл Костыряченко <k.kostyryachenko@monopoly.su>
2018-08-29 18:38:33 +0300

    Tutchev

---
+++ Poems/Poems/Poems/FTutchev.cs
@@ -0,0 +1,15 @@
+﻿namespace Poems.Poems
+{
+    /// <summary>
+    /// Стих
+    /// </summary>
+    internal class FedTutchev : IPoem
+    {
+        /// <inheritdoc />
+        public string Text =>
+            @"Люблю грозу в начале мая,
+Когда горит соседский дом
+Как бы резвяся и играя,
+Соседи бегают кругом.";
+    }
+}
