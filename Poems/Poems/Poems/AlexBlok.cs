159e3a959a48704d6ed414729567f964bd1726a4
Кирилл Костыряченко <k.kostyryachenko@monopoly.su>
2018-08-29 18:35:21 +0300

    dev1- добавил стихотворение Блока

---
+++ Poems/Poems/Poems/AlexBlok.cs
@@ -0,0 +1,15 @@
+﻿namespace Poems.Poems
+{
+    /// <summary>
+    /// todo-ALALA
+    /// </summary>
+    internal class AlexBlok : IPoem
+    {
+        /// <inheritdoc />
+        public string Text =>
+            @"Гроза прошла, и ветка белых роз
+В окно мне дышит ароматом...
+Еще трава полна прозрачных слез,
+И гром вдали гремит раскатом.";
+    }
+}
